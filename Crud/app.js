eb6f7dc01fd2724d8234b379f05bdd3fc123981e
Balamurugan84 <102845708+Balamurugan84@users.noreply.github.com>
2022-04-14 19:12:48 +0530

    Add files via upload

---
+++ Crud/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+//const envData = require('./env.json');
+//require('dotenv').config()
+const dressRoute = require('./routes/dress.routes');
+//const { kill } = require('nodemon/lib/monitor/run');
+const userRoutes = require('./routes/user.routes');
+
+const app = express();
+app.use(cors());
+
+app.get("/healthcheck", async(req,res)=>{
+    console.log("it works");
+   // process.exit(1);
+    res.send({status: 'Success'})
+})
+mongoose.connect('mongodb://localhost:27017/ecommerece', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(data=>{
+    console.log("database connected")
+}).catch(err=>{
+    console.log(err.message)
+   process.exit(1)
+})
+app.use(express.json());
+app.use('/api/v1/dress/',dressRoute);
+app.use('/api/v2/user/',userRoutes)
+
+app.listen(7075, ()=>{
+    console.log("start....")
+})
+
